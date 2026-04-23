b2ce9ef0b0b2d2309a514fe35688442dd3115f18
한성준 <june12300039@gmail.com>
2026-04-23 16:11:43 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+int main(){
+    fastio
+    int a = 0, b = 0;
+    cin >> a >> b;
+    cout << a - b;
+}
