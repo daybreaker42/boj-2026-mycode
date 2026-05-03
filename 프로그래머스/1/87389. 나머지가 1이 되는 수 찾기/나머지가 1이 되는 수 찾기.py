803fc9e6a4c3cab9514d29fec44483ca2abce1b3
한성준 <june12300039@gmail.com>
2026-05-04 04:35:36 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.14 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,14 +1,17 @@
 '''
 solved: 2026-05-04
 idea: n-1의 1이아닌 가장 작은 약수를 구함
-시간복잡도: O(sqrt(N))
+시간복잡도: O(N)
+체감난이도: 브2
 '''
 def solution(n):
     answer = 0
     i = 2
-    while i <= n - 1:
+    while i * i <= n - 1:
         if (n - 1) % i == 0:
             answer = i
             break
         i += 1
+    if answer == 0:
+        answer = n - 1
     return answer
